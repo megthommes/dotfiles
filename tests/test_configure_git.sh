ba928f584dfb62229d5fdc192ba718adde9717ed
Meghan Thommes <meghan.thommes@gmail.com>
2024-09-10 13:34:29 -0400

    update git setup to add gpg key

--- tests/test_configure_git.sh
+++ tests/test_configure_git.sh
@@ -1,5 +1,4 @@
 # Test that Git is configured
-
 echo "Testing that Git is configured..."
 
 # check if GitHub CLI is installed
@@ -21,15 +20,29 @@ echo "\t...SSH key file exists."
 # check if the SSH key is added to GitHub
 echo "\tChecking if the SSH key is added to GitHub..."
 PUBLIC_KEY=$(cat ~/.ssh/id_ed25519.pub | awk '{print $2}')
-# signing
-if ! gh ssh-key list | grep -Fw "$PUBLIC_KEY" | grep -q "signing"; then
-    echo "\t\tError: SSH key is not added to GitHub as a signing key."
-fi
 # authentication
 if ! gh ssh-key list | grep -Fw "$PUBLIC_KEY" | grep -q "authentication"; then
     echo "\t\tError: SSH key is not added to GitHub as an authentication key."
+else
+    ssh -T git@github.com
 fi
 echo "\t...done checking SSH key."
+
+# check if the GPG key file exists
+echo "\tChecking if the GPG key file exists..."
+GPG_KEY_ID=$(gpg --list-secret-keys --keyid-format=long | grep sec | awk '{print $2}' | awk -F'/' '{print $2}')
+if [ -z "$GPG_KEY_ID" ]; then
+    echo "\t\tError: No GPG key found."
+    exit 1
+fi
+echo "\t...GPG key file exists."
+
+# check if the GPG key is added to GitHub
+echo "\tChecking if the GPG key is added to GitHub..."
+if ! gh gpg-key list | grep -q "$GPG_KEY_ID"; then
+    echo "\t\tError: GPG key is not added to GitHub."
+fi
+echo "\t...done checking GPG key."
 
 # check if the submodules are installed
 echo "\tChecking if submodules are installed..."
